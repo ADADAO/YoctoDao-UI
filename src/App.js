7b61c6092f846ed03649e0437300bc8a5a4c50fb
Riley-Kilgore <rileyk9@icloud.com>
2021-12-18 13:05:30 -0800

    changed to browser instead of nodejs serialization lib

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import '@emurgo/cardano-serialization-lib-nodejs';
+import '@emurgo/cardano-serialization-lib-browser';
 
 function App() {
   return (
