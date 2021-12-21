bd607c464ee5f80b1b7ab9a57373934aab605115
Riley-Kilgore <rileyk9@icloud.com>
2021-12-21 09:27:05 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "yoctodao-interface",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
